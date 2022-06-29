d85dd5e7690c4842b0df38a23843490dce0820a9
Miguel Natan Tomaz <miguelnatantomaz@hotmail.com>
2022-06-29 17:45:27 -0300

    style: tema criado com styled-components

---
+++ src/styles/theme.ts
@@ -0,0 +1,12 @@
+export default {
+  colors: {
+    primary: "#12B629",
+    secondary: "#E3E3E3",
+    shape: "#FFFFFF",
+  },
+  fonts: {
+    title: "Lato-Bold",
+    text: "Ubuntu-Light",
+  },
+  fontSize: {},
+};
