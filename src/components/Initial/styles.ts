19e1e53d641d66ef3bea7aee51189236d56dd6e9
Miguel Natan Tomaz <miguelnatantomaz@hotmail.com>
2022-07-03 22:41:34 -0300

    style: atualizado estilo do component initial

--- src/components/Initial/styles.ts
+++ src/components/Initial/styles.ts
@@ -11,63 +11,59 @@ export const Container = styled.div`
   width: 100%;
   height: 100vh;
   align-items: center;
-
   div {
     width: 100%;
     max-width: 1200px;
     display: flex;
     justify-content: space-between;
-  }
+    align-items: center;
 
-  img {
-    width: 450px;
-  }
-
-  section {
-    display: flex;
-    flex-direction: column;
-
-    span {
-      font-family: ${({ theme }) => theme.fonts.title};
-      color: ${({ theme }) => theme.colors.primary};
-      font-size: 64px;
+    img {
+      width: 450px;
     }
-
-    h2 {
-      font-size: 82px;
-      margin: 0px;
-      color: ${({ theme }) => theme.colors.shape};
-    }
-
-    h1 {
-      font-family: ${({ theme }) => theme.fonts.title};
-      color: ${({ theme }) => theme.colors.secondary};
-      font-size: 32px;
-    }
-    @media (max-width: 644px) {
+    section {
+      display: flex;
+      flex-direction: column;
       span {
-        font-size: 48px;
+        font-family: ${({ theme }) => theme.fonts.title};
+        color: ${({ theme }) => theme.colors.primary};
+        font-size: 64px;
       }
       h2 {
-        font-size: 60px;
+        font-size: 82px;
+        margin: 0px;
+        color: ${({ theme }) => theme.colors.shape};
+        font-family: ${({ theme }) => theme.fonts.title};
       }
-    }
-    @media (max-width: 478px) {
-      span {
+      h1 {
+        font-family: ${({ theme }) => theme.fonts.title};
+        color: ${({ theme }) => theme.colors.secondary};
         font-size: 32px;
       }
-      h2 {
-        font-size: 42px;
+      @media (max-width: 644px) {
+        span {
+          font-size: 48px;
+        }
+        h2 {
+          font-size: 60px;
+        }
+      }
+      @media (max-width: 478px) {
+        span {
+          font-size: 32px;
+        }
+        h2 {
+          font-size: 42px;
+        }
       }
     }
-  }
-  @media (max-width: 1024px) {
-    flex-direction: column;
-  }
-
-  @media (max-width: 478px) {
-    img {
-      width: 278px;
+    @media (max-width: 1024px) {
+      flex-direction: column;
+    }
+    @media (max-width: 478px) {
+      img {
+        width: 278px;
+      }
     }
   }
 `;
