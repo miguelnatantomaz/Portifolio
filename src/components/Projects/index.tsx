a6c75e9c0ff9501cfcd2c2bceb919361fdf42ac7
Miguel Natan Tomaz <miguelnatantomaz@hotmail.com>
2022-07-03 22:40:16 -0300

    feat: feito o componente Projects

---
+++ src/components/Projects/index.tsx
@@ -0,0 +1,38 @@
+import { Container, CardProjects } from "./styles";
+
+import { Button } from "../Button";
+import { useProjects } from "../../Providers/Projects";
+import { IProjects } from "../../Providers/Projects/interface";
+
+const Projects = () => {
+  const { projects }: any = useProjects();
+  console.log(projects);
+
+  return (
+    <>
+      <Container>
+        <span id="projects"></span>
+        <h2>Projetos</h2>
+        <section>
+          <ul>
+            {projects.map((project: IProjects) => (
+              <CardProjects key={project.name}>
+                <h3>{project.name}</h3>
+                <img src={project.img} alt="" />
+                <p>{project.description}</p>
+                <a href={project.aplication} target="_blank" rel="noreferrer">
+                  <Button>Aplicação</Button>
+                </a>
+                <a href={project.repository} target="_blank" rel="noreferrer">
+                  <Button>Repositório</Button>
+                </a>
+              </CardProjects>
+            ))}
+          </ul>
+        </section>
+      </Container>
+    </>
+  );
+};
+
+export default Projects;
---
+++ src/components/Projects/styles.ts
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  #projects {
+    margin-bottom: 65px;
+  }
+
+  h2 {
+    font-size: 42px;
+    color: ${({ theme }) => theme.colors.primary};
+    padding: 5px 20px;
+    border-bottom: solid 4px ${({ theme }) => theme.colors.secondary};
+    font-family: ${({ theme }) => theme.fonts.title};
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: column;
+    align-items: center;
+    padding: 0px;
+  }
+`;
+
+export const CardProjects = styled.li`
+  width: 350px;
+  padding: 20px;
+  background-color: ${({ theme }) => theme.colors.secondary};
+  border-radius: 20px;
+  margin: 20px 0px;
+
+  :hover {
+    transition: 0.5s;
+    transform: scale(1.1);
+  }
+
+  h3 {
+    font-family: ${({ theme }) => theme.fonts.title};
+    font-size: 26px;
+  }
+
+  img {
+    width: 100%;
+    height: 250px;
+  }
+  p {
+    width: 100%;
+    font-family: ${({ theme }) => theme.fonts.text};
+  }
+  button {
+    margin: 0px 10px;
+  }
+
+  @media (max-width: 420px) {
+    width: 80%;
+    button {
+      margin: 0px 2px;
+    }
+  }
+`;
