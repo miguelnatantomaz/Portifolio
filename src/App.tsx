98aa301600c2300566a728a9236d856552b78613
Miguel Natan Tomaz <miguelnatantomaz@hotmail.com>
2022-07-03 22:44:34 -0300

    feat: adicionado fonts.css ao App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import Routes from "./Routes";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./styles/global";
+import Provider from "./Providers";
+import "./styles/fonts.css";
 
 import theme from "./styles/theme";
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Routes />
-    </ThemeProvider>
+    <>
+      <Provider>
+        <ThemeProvider theme={theme}>
+          <GlobalStyle />
+          <Routes />
+        </ThemeProvider>
+      </Provider>
+    </>
   );
 }
 
